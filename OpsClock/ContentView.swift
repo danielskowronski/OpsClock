0a9c44a1576211a46ea72932b63dc70c1b09bdb2
Daniel Skowroński <daniel@skowron.ski>
2022-12-29 13:16:26 +0100

    macOS single tab view with timer updated clock and float on start

--- OpsClock/ContentView.swift
+++ OpsClock/ContentView.swift
@@ -8,7 +8,52 @@
 import SwiftUI
 import CoreData
 
+struct MainClockView: View {
+    @State var date = Date()
+    
+
+    
+    var body: some View {
+        HStack (alignment: .firstTextBaseline) {
+            Text("\(mainClockString(date: date))")
+                .font(
+                    .custom(
+                        "Ubuntu Mono",
+                        size: 100)
+                )
+            
+            Text("\(millisecondClockString(date: date))")
+                .font(
+                    .custom(
+                        "Ubuntu Mono",
+                        size: 50)
+                )
+        }
+        .onAppear(perform: {
+            let _ = self.updateTimer;
+            
+            
+        })
+        .onReceive(NotificationCenter.default.publisher(for: NSWindow.didBecomeKeyNotification)) { notification in
+            if let window = notification.object as? NSWindow {
+                window.level = .floating
+            }
+        }
+        
+        
+    }
+    
+    var updateTimer: Timer {
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true,
+                             block: {_ in
+                                 self.date = Date()
+            
+                               })
+    }
+}
+
 struct ContentView: View {
+    
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
@@ -17,71 +62,40 @@ struct ContentView: View {
     private var items: FetchedResults<Item>
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        TabView {
+            MainClockView()
+                .tabItem {
+                    Label("Clock", systemImage: "clock")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+    
+    
+    
 }
 
-private let itemFormatter: DateFormatter = {
+var mainClockFormatter: DateFormatter {
     let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
+    formatter.dateFormat = "hh:mm:ss"
     return formatter
-}()
+}
+func mainClockString(date: Date) -> String {
+     let time = mainClockFormatter.string(from: date)
+     return time
+}
+
+var millisecondClockFormatter: DateFormatter {
+    let formatter = DateFormatter()
+    formatter.dateFormat = ".S"
+    return formatter
+}
+func millisecondClockString(date: Date) -> String {
+     let time = millisecondClockFormatter.string(from: date)
+     return time
+}
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
