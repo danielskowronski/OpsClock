7e2d2f0fd3e15818829a64a01eaeafd26f224111
Daniel Skowroński <daniel@skowron.ski>
2022-12-29 16:16:23 +0100

    macOS smarter approach to text formatting removing other issues; icons & packages

--- OpsClock/ContentView.swift
+++ OpsClock/ContentView.swift
@@ -7,106 +7,60 @@
 
 import SwiftUI
 import CoreData
+import AttributedText
 
 struct MainClockView: View {
     @State var date = Date()
-        
-    let bigFont = Font.custom("Ubuntu Mono", size: 2048, relativeTo: .largeTitle)
-    let smallFont = Font.custom("Ubuntu Mono", size: 1024, relativeTo: .largeTitle)
+    
     var updateTimer: Timer {
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true,
                              block: {_ in
-                                 self.date = Date()
-            
-                               })
+            self.date = Date()
+        })
     }
     
-    
-    let ratioOfLargeClock = 8.0/9; // "12:34:56.7" is LLLLLLLLSS, wher L=large font, S=small font, which is 0.5 of L
-    let ratioOfHeighToWidth = (1.0/9) / (1.0/2); // 1/9 is ratio of L-glyph width to window width, 1/2 is width to height ratio of Ubuntu Mono - see https://ubuntu.com/blog/ubuntu-monospace-beta
-    
     var body: some View {
-        GeometryReader { geometry in
-            HStack (alignment: .firstTextBaseline, spacing: 0) {
-                Group {
-                    Text("\(mainClockString(date: date))")
-                        .font(bigFont)
-                        .frame(width: geometry.size.width * (ratioOfLargeClock))
-                        .frame(height: geometry.size.width * (ratioOfHeighToWidth))
-                    
-                    Text("\(millisecondClockString(date: date))")
-                        .font(smallFont)
-                        .frame(width: geometry.size.width * (1 - ratioOfLargeClock))
-                        .frame(height: geometry.size.width * (ratioOfHeighToWidth))
-                }
-                .minimumScaleFactor(0.01)
-            }
-            
+        AttributedText("\(clockString(date: date))") //Attributed
+            .scaledToFit()
+            .minimumScaleFactor(0.001)
             .onAppear(perform: {
                 let _ = self.updateTimer;
-                
-                
             })
             .onReceive(NotificationCenter.default.publisher(for: NSWindow.didBecomeKeyNotification)) { notification in
                 if let window = notification.object as? NSWindow {
                     window.level = .floating
-                    //window.aspectRatio = NSSize(width: 9, height: 2.1)
                 }
             }
-            
-            
-            
-        }
     }
-    
 }
 
-struct ContentView: View {
-    
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+struct ContentView: View {   
     var body: some View {
         TabView {
             MainClockView()
                 .tabItem {
                     Label("Clock", systemImage: "clock")
-                }.aspectRatio(9.0/2.5, contentMode: .fit).padding(20)
+                }.padding(20)
         }
     }
-        
-    
-    func windowWillResize(sender: NSWindow, toSize frameSize: NSSize) -> NSSize { let mySize = NSSize(width: frameSize.width, height: frameSize.width * 2); return mySize }
-
-    
 }
 
-var mainClockFormatter: DateFormatter {
+var clockFormatter: DateFormatter {
     let formatter = DateFormatter()
     formatter.dateFormat = "HH:mm:ss"
     return formatter
 }
-func mainClockString(date: Date) -> String {
-     let time = mainClockFormatter.string(from: date)
-     return time
-}
-
-var millisecondClockFormatter: DateFormatter {
+var milliFormatter: DateFormatter {
     let formatter = DateFormatter()
     formatter.dateFormat = ".S"
     return formatter
 }
-func millisecondClockString(date: Date) -> String {
-     let time = millisecondClockFormatter.string(from: date)
-     return time
+
+func clockString(date: Date) -> String {
+    let clockString = clockFormatter.string(from: date);
+    let milliString = milliFormatter.string(from: date);
+    return "<large>"+clockString+"</large><small>"+milliString+"</small>";
 }
-
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- OpsClock/OpsClockApp.swift
+++ OpsClock/OpsClockApp.swift
@@ -6,16 +6,35 @@
 //
 
 import SwiftUI
+import AttributedText
 
 @main
 struct OpsClockApp: App {
     let persistenceController = PersistenceController.shared
-
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+    }
+    
+    init() {
+        AttributedText.tags = [
+            "large": { $0.font(Font.custom("Ubuntu Mono Bold", size: 2048)) },
+            "small": { $0.font(Font.custom("Ubuntu Mono Bold", size: 1024)) },
+        ]
+        /*
+         AttributedText with custom tags used here because:
+            - can't have multiple Text objects in HStack that would scale together natively without grid
+              (i.e. one of font size 2x and other of 1x, without ton of shitty event processing)
+            - but with grid the font scaling is always clipping to proper font sizes (integers and not all of them)
+              so text alignement tends to break on smaller sizes,
+              not to mention issues with frame scaling and aspect ratio
+            - can't use NSAttributedString as NSAttributedStringKey.font expects value of UIFont,
+              which is part of UIKit which is not available on macOS
+            - also markdown renderer can't handle multiple sizes in single line
+         */
     }
     
 }
